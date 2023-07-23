aaecce6f5d56c744fdd0a4e250ffe25ca3db1d2d
wangyan <1536719793@qq.com>
2023-07-24 01:04:40 +0800

    第一节练习

---
+++ basic_syntax/basic_type/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	var a int = 64
+	var b int = 8
+	println(a + b)
+	println(a - b)
+	println(a * b)
+	if b != 0 {
+		println(a / b)
+	}
+	//var c float64 = 12.3
+	//println(a + c)
+	//不同类型的无法进行计算
+	//var d int32 = 12
+	//println(a + d)
+	string()
+}
---
+++ basic_syntax/basic_type/string.go
@@ -0,0 +1,5 @@
+package main
+
+func string() {
+	println("Hello,\" GO!")
+}
